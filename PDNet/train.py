a28897190b96c51e10726902f6ceab72106c7310
NIC-CJC <135231302+ChenJiaCong-1005@users.noreply.github.com>
2024-01-19 15:13:00 +0800

    Add files via upload

---
+++ PDNet/train.py
@@ -0,0 +1,168 @@
+import numpy as np
+import torch
+import argparse
+import cv2
+import torch.utils.data as data
+import torchvision
+import random
+import torch.nn.functional as F
+import torch.nn as nn
+from tensorboardX import SummaryWriter
+import torch.optim as optim
+import os
+from model.model import model_fn_decorator
+from model.nets_student import my_model
+from model.nets_teacher import T_my_model
+from model import *
+from dataset.load_data import *
+from tqdm import tqdm
+from utils.loss_util import *
+from utils.common import *
+import lpips
+from config.config import args
+
+def train_epoch(args, TrainImgLoader, model, T_model, model_fn, optimizer, epoch, iters, lr_scheduler):
+    """
+    Training Loop for each epoch
+    """
+    tbar = tqdm(TrainImgLoader)
+    total_loss = 0
+    lr = optimizer.state_dict()['param_groups'][0]['lr']
+    for batch_idx, data in enumerate(tbar):
+        loss = model_fn(args, data, model, T_model, iters)
+        # backward and update
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        iters += 1
+        total_loss += loss.item()
+        avg_train_loss = total_loss / (batch_idx+1)
+        desc = 'Training  : Epoch %d, lr %.7f, Avg. Loss = %.5f' % (epoch, lr, avg_train_loss)
+        tbar.set_description(desc)
+        tbar.update()
+    lr = optimizer.state_dict()['param_groups'][0]['lr']
+    # the learning rate is adjusted after each epoch
+    lr_scheduler.step()
+
+    return lr, avg_train_loss, iters
+
+def init():
+    # Make dirs
+    args.LOGS_DIR = os.path.join(args.SAVE_PREFIX, args.EXP_NAME, 'logs')
+    args.NETS_DIR = os.path.join(args.SAVE_PREFIX, args.EXP_NAME, 'net_checkpoints')
+    args.VISUALS_DIR = os.path.join(args.SAVE_PREFIX, args.EXP_NAME, 'train_visual')
+    mkdir(args.LOGS_DIR)
+    mkdir(args.NETS_DIR)
+    mkdir(args.VISUALS_DIR)
+
+    #os.environ["CUDA_VISIBLE_DEVICES"] = "%d" % args.GPU_ID
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"  #change GPU ID
+    '''device = torch.cuda.set_device(0)
+    print(device)'''
+    device1 = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+    print(device1)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(device)
+
+    # random seed
+    random.seed(args.SEED)
+    np.random.seed(args.SEED)
+    torch.manual_seed(args.SEED)
+    torch.cuda.manual_seed_all(args.SEED)
+    if args.SEED == 0:
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+    else:
+        torch.backends.cudnn.deterministic = False
+        torch.backends.cudnn.benchmark = True
+
+    # summary writer
+    logger = SummaryWriter(args.LOGS_DIR)
+    
+    return logger, device, device1
+
+def load_teacher(device):
+    print("Loading Teacher ===============================")
+    T_net = T_my_model(args.T_en_f, args.T_en_i,
+                                      args.T_de_f, args.T_de_i, 
+                                      args.T_sam).to(device)
+    T_net.load_state_dict(torch.load(args.T_load_path)['state_dict'])
+    for p in T_net.parameters():
+        p.requires_grad = False
+    return T_net
+
+def load_checkpoint(model, optimizer, load_epoch):
+    load_dir = args.NETS_DIR + '/checkpoint' + '_' + '%06d' % load_epoch + '.tar'
+    print('Loading pre-trained checkpoint %s' % load_dir)
+    model_state_dict = torch.load(load_dir)['state_dict']
+    model.load_state_dict(model_state_dict)
+    optimizer_dict = torch.load(load_dir)['optimizer']
+    optimizer.load_state_dict(optimizer_dict)
+    learning_rate = torch.load(load_dir)['learning_rate']
+    iters = torch.load(load_dir)['iters']
+    print('Learning rate recorded from the checkpoint: %s' % str(learning_rate))
+
+    return learning_rate, iters
+
+def main():
+    logger, device, device1 = init()
+    # create model
+    model = my_model(en_feature_num=args.EN_FEATURE_NUM,
+                     en_inter_num=args.EN_INTER_NUM,
+                     de_feature_num=args.DE_FEATURE_NUM,
+                     de_inter_num=args.DE_INTER_NUM,
+                     sam_number=args.SAM_NUMBER,
+                     ).to(device)
+    model._initialize_weights()
+
+    # create optimizer
+    optimizer = optim.Adam([{'params': model.parameters(), 'initial_lr': args.BASE_LR}], betas=(0.9, 0.999))
+    learning_rate = args.BASE_LR
+    iters = 0
+    # resume training
+    if args.LOAD_EPOCH:
+        learning_rate, iters = load_checkpoint(model, optimizer, args.LOAD_EPOCH)
+    # create loss function
+    loss_fn = multi_VGGPerceptualLoss(lam=args.LAM, lam_p=args.LAM_P).to(device)
+    # create learning rate scheduler
+    lr_scheduler = CosineAnnealingWarmRestarts(optimizer, T_0=args.T_0, T_mult=args.T_MULT, eta_min=args.ETA_MIN,
+                                               last_epoch=args.LOAD_EPOCH - 1)
+    # create training function
+    model_fn = model_fn_decorator(loss_fn=loss_fn, device=device, device1=device1)
+    # create dataset
+    train_path = args.TRAIN_DATASET
+    TrainImgLoader = create_dataset(args, data_path=train_path, mode='train')
+    
+    T_model = load_teacher(device=device1)
+
+    # start training
+    print("****start traininig!!!****")
+    avg_train_loss = 0
+    for epoch in range(args.LOAD_EPOCH + 1, args.EPOCHS + 1):
+        learning_rate, avg_train_loss, iters = train_epoch(args, TrainImgLoader, model, T_model, model_fn, optimizer, epoch,
+                                                           iters, lr_scheduler)
+        logger.add_scalar('Train/avg_loss', avg_train_loss, epoch)
+        logger.add_scalar('Train/learning_rate', learning_rate, epoch)
+
+        # Save the network per ten epoch
+        if epoch % 10 == 0:
+            savefilename = args.NETS_DIR + '/checkpoint' + '_' + '%06d' % epoch + '.tar'
+            torch.save({
+                'learning_rate': learning_rate,
+                'iters': iters,
+                'optimizer': optimizer.state_dict(),
+                'state_dict': model.state_dict()
+            }, savefilename)
+
+        # Save the latest model
+        savefilename = args.NETS_DIR + '/checkpoint' + '_' + 'latest.tar'
+        torch.save({
+            'learning_rate': learning_rate,
+            'iters': iters,
+            'optimizer': optimizer.state_dict(),
+            'state_dict': model.state_dict()
+        }, savefilename)
+
+
+if __name__ == '__main__':
+    main()
---
+++ PDNet/utils/loss_util.py
@@ -0,0 +1,106 @@
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import torchvision
+from utils.common import *
+from torchvision import models as tv
+from torch.nn.parameter import Parameter
+import os
+
+
+
+class multi_VGGPerceptualLoss(torch.nn.Module):
+    def __init__(self, lam=1, lam_p=1):
+        super(multi_VGGPerceptualLoss, self).__init__()
+        self.loss_fn = VGGPerceptualLoss()
+        self.lam = lam
+        self.lam_p = lam_p
+        self.dwt = DWT()
+    def forward(self, out1, out2, out3, gt1, feature_map, Tout_1, Tout_2, Tout_3, T_feature_map, feature_layers=[2]):
+        gt2 = F.interpolate(gt1, scale_factor=0.5, mode='bilinear', align_corners=False)
+        gt3 = F.interpolate(gt1, scale_factor=0.25, mode='bilinear', align_corners=False)
+        
+        loss1 = self.lam_p*self.loss_fn(out1, gt1, feature_layers=feature_layers) + self.lam*F.l1_loss(out1, gt1)
+        loss2 = self.lam_p*self.loss_fn(out2, gt2, feature_layers=feature_layers) + self.lam*F.l1_loss(out2, gt2)
+        loss3 = self.lam_p*self.loss_fn(out3, gt3, feature_layers=feature_layers) + self.lam*F.l1_loss(out3, gt3)
+        
+        l1_s = F.l1_loss(out1, gt1)
+        l1_t = F.l1_loss(Tout_1, gt1)
+        
+        if l1_s > l1_t: 
+            a = 1
+        else:
+            a = 0
+        
+        feature_loss = 0
+        i = 0
+        for fm in feature_map:
+            loss = F.l1_loss(self.dwt(fm), self.dwt(T_feature_map[i]))
+            i += 1
+            feature_loss += loss
+        return loss1+loss2+loss3+40*a*feature_loss            
+
+def dwt_init(x):
+    x01 = x[:, :, 0::2, :] / 2
+    x02 = x[:, :, 1::2, :] / 2
+    x1 = x01[:, :, :, 0::2]
+    x2 = x02[:, :, :, 0::2]
+    x3 = x01[:, :, :, 1::2]
+    x4 = x02[:, :, :, 1::2]
+    x_LL = x1 + x2 + x3 + x4
+    x_HL = -x1 - x2 + x3 + x4
+    x_LH = -x1 + x2 - x3 + x4
+    x_HH = x1 - x2 - x3 + x4
+
+    return torch.cat((x_LL, x_HL, x_LH, x_HH), 1)
+
+class DWT(nn.Module):
+    def __init__(self):
+        super(DWT, self).__init__()
+        self.requires_grad = False
+
+    def forward(self, x):
+        return dwt_init(x)
+
+class VGGPerceptualLoss(torch.nn.Module):
+    def __init__(self, resize=True):
+        super(VGGPerceptualLoss, self).__init__()
+        blocks = []
+        blocks.append(torchvision.models.vgg16(pretrained=True).features[:4].eval())
+        blocks.append(torchvision.models.vgg16(pretrained=True).features[4:9].eval())
+        blocks.append(torchvision.models.vgg16(pretrained=True).features[9:16].eval())
+        blocks.append(torchvision.models.vgg16(pretrained=True).features[16:23].eval())
+        for bl in blocks:
+            for p in bl:
+                p.requires_grad = False
+        self.blocks = torch.nn.ModuleList(blocks)
+        self.transform = torch.nn.functional.interpolate
+        self.mean = torch.nn.Parameter(torch.tensor([0.485, 0.456, 0.406]).view(1,3,1,1))
+        self.std = torch.nn.Parameter(torch.tensor([0.229, 0.224, 0.225]).view(1,3,1,1))
+        self.resize = resize
+
+    def forward(self, input, target, feature_layers=[0, 1, 2, 3], style_layers=[]):
+        if input.shape[1] != 3:
+            input = input.repeat(1, 3, 1, 1)
+            target = target.repeat(1, 3, 1, 1)
+        input = (input-self.mean) / self.std
+        target = (target-self.mean) / self.std
+        if self.resize:
+            input = self.transform(input, mode='bilinear', size=(224, 224), align_corners=False)
+            target = self.transform(target, mode='bilinear', size=(224, 224), align_corners=False)
+        loss = 0.0
+        x = input
+        y = target
+        for i, block in enumerate(self.blocks):
+            x = block(x)
+            y = block(y)
+            if i in feature_layers:
+                loss += torch.nn.functional.l1_loss(x, y)
+            if i in style_layers:
+                act_x = x.reshape(x.shape[0], x.shape[1], -1)
+                act_y = y.reshape(y.shape[0], y.shape[1], -1)
+                gram_x = act_x @ act_x.permute(0, 2, 1)
+                gram_y = act_y @ act_y.permute(0, 2, 1)
+                loss += torch.nn.functional.l1_loss(gram_x, gram_y)
+        return loss
